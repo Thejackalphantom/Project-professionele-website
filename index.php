f5cbc247d3ad2d957ef78f4a919fa2b83e479fd4
DESKTOP-BQHDEF9\Remy Conen <remy.conen@hotmail.com>
2018-10-05 12:46:09 +0200

    Footer toegevoegd en een logo div ingesteld

--- index.php
+++ index.php
@@ -7,27 +7,13 @@ Index Project Professionele Website
         <link rel="stylesheet" type="text/css" href="index.css">
         <meta charset="UTF-8">
         <title>placeholder</title>
-        <script>
-            var coll = document.getElementsByClassName("collapsible");
-            var i;
-
-            for (i = 0; i < coll.length; i++) {
-              coll[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var content = this.nextElementSibling;
-                if (content.style.maxHeight){
-                  content.style.maxHeight = null;
-                } else {
-                  content.style.maxHeight = content.scrollHeight + "px";
-                } 
-              });
-            }
-        </script>
+        
     </head>
     <body>
         <div id="mainContainer">
             <div id="sideBarLeft">
-               
+                <div id="logo">
+                </div>
             </div>
             <div id="header">
                 
@@ -36,6 +22,9 @@ Index Project Professionele Website
             </div>
             <div id="sideBarRight">
             </div>
+            <div id="footer">
+                <span> &copy; Archon Hogeschool en het Archon Hogeschool logo zijn beschermde 
+            </div>
         </div>
-    </body>
+     </body>
 </html>
