51ee05afbcc126d00069ef5c82a8baab256d004d
THIJSLAPTOP\Gebruiker <thijsrijkers@live.nl>
2018-10-17 14:04:28 +0200

    Content added to the footer, search bar moved to the footer, and added the social media links. (Overall design for the footer)

--- index.php
+++ index.php
@@ -50,9 +50,6 @@ Index Project Professionele Website
                 <div class="headerWhiteSpace2">
                 </div>
                 <div class="searchSpace">
-                    <div class="search">
-                        <input type="text" placeholder="Search..">
-                    </div>
                 </div>
                 <div id="menuSpace">
                 </div>
@@ -62,7 +59,7 @@ Index Project Professionele Website
                             <div id="hamburgerBox">
                             <img src="img/hamburger.jpg" alt="hamburger_menu">
                             </div>
-                            <p id="menuTextChange"> MENU </p>
+                            <p id="TextChange"> MENU </p>
                             <div class="dropdown-content">
                               <a href="www.google.com"> <p><?=CONTENT_HAMBURGER_NEWS; ?></p> </a>
                               <a href="www.google.com"> <p><?=CONTENT_MENU_1; ?></p> </a>
@@ -88,7 +85,54 @@ Index Project Professionele Website
             <div id="sideBarRight">
             </div>
             <div id="footer">
-                <span><?=CONTENT_FOOTER_DISCLAIMER; ?></span>
+
+                <div id="footerBlankSpaceUp">   
+                </div>
+                
+                <div id="footerBlankSpaceLeft">   
+                </div>
+                
+                <div id="footerContainer"> 
+                    <div id="footerInfo">   
+                        <p class="TextChangeWhite"> Over Archon Hogeschool </p>
+                    </div>
+                    <div id="footerInfo">   
+                        <p class="TextChangeWhite"> Contact </p>
+                    </div>
+                    <div id="footerInfo">   
+                        <p class="TextChangeWhite"> Disclaimer </p>
+                    </div>
+                    <div id="footerInfo"> 
+                        <p class="TextChangeWhite"> <?=CONTENT_FOOTER_DISCLAIMER; ?></p>
+                    </div>
+                </div>
+                
+                <div id="footerSpacer">   
+                </div>
+
+                <div id="footerSpaceLeftOfSpacer">   
+                </div>
+                
+                <div id="footerContainerSecond">
+                
+                <div class="search">
+                    <input type="text" placeholder="Search..">
+                </div>
+                <div id="footerRight">
+                    <div class="footerIcon">
+			<a href="http://facebook.nl" target="_blank"><img src="img/facebook.png" alt="Facebook"></a>
+                    </div>
+                    <div class="footerIcon">
+			<a href="http://twitter.com" target="_blank"><img src="img/twitter.png" alt="Twitter"></a>
+                    </div>
+                    <div class="footerIcon">
+			<a href="https://www.instagram.com" target="_blank"><img src="img/insta.png" alt="Instagram"></a>
+                    </div>
+                </div>
+                
+                </div>
+
+                
             </div> <!-- END OF FOOTER-->
         </div> <!-- END OF MAIN CONTAINERS-->
      </body>
