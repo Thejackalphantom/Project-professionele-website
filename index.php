c7a61b0a860e8824bcd977db88406cfa6dcbada3
Matta <pietertjepost123@gmail.com>
2018-10-17 14:16:48 +0200

    Update index.php
    added link to enroll in the menu

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ Index Project Professionele Website
                 <div id="menuFiller">
                     <div id="menu">
                         <p><?=CONTENT_MENU_1; ?></p>
-                        <p><?=CONTENT_MENU_2; ?></p>
+                        <p><a href='inschrijven.php'><?=CONTENT_MENU_2; ?></a></p>
                         <p><?=CONTENT_MENU_3; ?></p>
                         <p><?=CONTENT_MENU_4; ?></p>
                     </div>
