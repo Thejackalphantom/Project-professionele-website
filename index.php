20e3479cd27786a4acddc23571763214206dc1b2
Danny de Vries <43206429+DannyDeVries2311@users.noreply.github.com>
2018-10-17 13:05:56 +0200

    Further Indenting.

--- index.php
+++ index.php
@@ -58,20 +58,20 @@ Index Project Professionele Website
                 </div>
                 
                 <div class="dropdown" id="hamburger">
-                        <div class="dropdown">
-                            <div id="hamburgerBox">
-                            <img src="img/hamburger.jpg" alt="hamburger_menu">
-                            </div>
-                            <p id="menuTextChange"> MENU </p>
-                            <div class="dropdown-content">
-                              <a href="www.google.com"> <p><?=CONTENT_HAMBURGER_NEWS; ?></p> </a>
-                              <a href="www.google.com"> <p><?=CONTENT_MENU_1; ?></p> </a>
-                              <a href="www.google.com"> <p><?=CONTENT_MENU_2; ?></p> </a> 
-                              <a href="www.google.com"> <p><?=CONTENT_MENU_3; ?></p> </a>
-                              <a href="www.google.com"> <p><?=CONTENT_MENU_4; ?></p> </a>
-                            </div>
+                    <div class="dropdown">
+                        <div id="hamburgerBox">
+                        <img src="img/hamburger.jpg" alt="hamburger_menu">
+                        </div>
+                        <p id="menuTextChange"> MENU </p>
+                        <div class="dropdown-content">
+                            <a href="www.google.com"> <p><?=CONTENT_HAMBURGER_NEWS; ?></p> </a>
+                            <a href="www.google.com"> <p><?=CONTENT_MENU_1; ?></p> </a>
+                            <a href="www.google.com"> <p><?=CONTENT_MENU_2; ?></p> </a> 
+                            <a href="www.google.com"> <p><?=CONTENT_MENU_3; ?></p> </a>
+                            <a href="www.google.com"> <p><?=CONTENT_MENU_4; ?></p> </a>
                         </div>
                     </div>
+                </div>
                 
                   <div id="hamburgerRightSpace">
                   </div>
