6e1cae1e08f70970404a2c9e25828f29f6f9a2ec
DESKTOP-EVT4D6F\Dany Bruinsma <danybruinsma.chelsea@gmail.com>
2018-10-18 12:01:40 +0200

    add text, +position, add a title and position

--- Fotoalbum.php
+++ Fotoalbum.php
@@ -24,11 +24,15 @@ Fotoalbum Project Professionele Website
                 <?php include('includes/header.php');?>
             
             <div id="boxfotos">
+         
             <div id="middelContainer">
              <div class="fotosize">
+                 <h1> Fotoalbums </h1>
                  <img src="img/file.jpg" alt="file">
-                 
+                  <div class="textinbox">
+                  <p> Onze school </p>
              </div>
+                </div>
             </div> <!-- END OF MIDDLE CONTAINERS-->
             </div>
             <div id="sideBarRight">
