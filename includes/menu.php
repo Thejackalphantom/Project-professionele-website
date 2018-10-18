9fd2a0e105670da80740cae1115555f1564a49ef
Matta <pietertjepost123@gmail.com>
2018-10-18 12:14:16 +0200

    added menu link

--- includes/menu.php
+++ includes/menu.php
@@ -2,7 +2,7 @@
     <div id="menu">
       <p><?=CONTENT_MENU_1; ?></p>
       <p><a href='inschrijven.php'><?=CONTENT_MENU_2; ?></a></p>
-      <p><?=CONTENT_MENU_3; ?></p>
+      <p><a href='opleidingen.php'><?=CONTENT_MENU_3; ?></a></p>
       <p><?=CONTENT_MENU_4; ?></p>
     </div>
 </div>
