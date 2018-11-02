e22e0dbe7c10559b8bbb2c80419c7e5e3ecd325b
DESKTOP-BQHDEF9\Remy Conen <remy.conen@hotmail.com>
2018-11-02 15:27:04 +0100

    I have deleted all unused code in the CSS file for the header fixed the footer which is now responsive and fixed an issue with the Header poppin the whole website away once it got rescaled still need to edit this to work as normal and not using a cheesy way like it is now.

--- includes/header.php
+++ includes/header.php
@@ -1,52 +1,37 @@
 <div id="header">
-    <div id="headerContainer">
-    </div>
-     <a href='inlog.php'> <div class="headerInlog">
-        <p class="biggerText"><?php echo $lang['login']; ?></p>
-        
-         </div> </a>
-    <div id="headerSpace">
-    </div>
-    <div class="headerLang">
-        <?php 
-            if(isset($_GET['lang'])){
-                if($_GET['lang'] == "nl"){
-                    echo "<a href='?lang=en'>Verander taal</a>";
+    <div id="navMenu">
+        <a href='inlog.php'> 
+            <div class="navInlog">
+                <p class="biggerText"><?php echo $lang['login']; ?></p>
+            </div> 
+        </a>
+        <div id="navSpace">
+        </div>
+        <div class="navLang">
+            <?php 
+                if(isset($_GET['lang'])){
+                    if($_GET['lang'] == "nl"){
+                        echo "<a href='?lang=en'>English</a>";
+                    }else{
+                        echo "<a href='?lang=nl'>Nederlands</a>";
+                    }
                 }else{
-                    echo "<a href='?lang=nl'>Change Language</a>";
+                    echo "<a href='?lang=en'>English</a>";
                 }
-            }else{
-                echo "<a href='?lang=en'>Verander taal</a>";
-            }
-        
-        ?>
-    </div> <!-- END OF HEADER INLOG-->
-    <div id="headerContainer2">
-    </div>
-    <div class="headerWhiteSpace">
-    </div>
-    <div class="headerWhiteSpace2">
-    </div>
-    <div class="searchSpace">
-    </div>
-    <div id="menuSpace">
-    </div>
-    <!--                HEAD-->
-    <div class="dropdown" id="hamburger">
-        <div class="dropdown">
-            <div id="hamburgerBox">
-                <img src="img/hamburger.jpg" alt="hamburger_menu">
-            </div>
-                <p id="TextChange"> MENU </p>
-            <div class="dropdown-content">
-                <a href="nieuws.php"> <p><?php echo $lang['news'];?></p> </a>
-                <a href="Fotoalbum.php"> <p><?php echo $lang['photoalbum'];?></p> </a>
-                <a href="specialNeeds.php"> <p><?php echo $lang['special_education'];?></p> </a>
+            ?>
+        </div>
+        <div class="dropdown" id="hamburger">
+            <div class="dropdown">
+                <div id="hamburgerBox">
+                    <img src="img/hamburger.jpg" alt="hamburger_menu">
+                </div>
+                    <p id="TextChange"> MENU </p>
+                <div class="dropdown-content">
+                    <a href="nieuws.php"> <p><?php echo $lang['news'];?></p> </a>
+                    <a href="Fotoalbum.php"> <p><?php echo $lang['photoalbum'];?></p> </a>
+                    <a href="specialNeeds.php"> <p><?php echo $lang['special_education'];?></p> </a>
+                </div>
             </div>
         </div>
-    </div>
-    <div id="hamburgerRightSpace">
-    </div>
-    <div id="hamburgerUnderSpace">
-    </div>
-</div> <!-- END OF HEADER -->
+    </div> <!-- END OF HEADER -->
+</div>
